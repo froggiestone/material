b48731c5c18cb16125939ce1e99c62f69ed641fb
Matthew Findley <findley@substantial.com>
2015-05-04 17:06:59 -0700

    docs(codepen): add 'the why' comments

--- docs/app/js/codepen.js
+++ docs/app/js/codepen.js
@@ -3,6 +3,12 @@
     .factory('$codepenDataAdapter', CodepenDataAdapter)
     .factory('$codepen', ['$demoAngularScripts', '$document', '$codepenDataAdapter', Codepen]);
 
+  /**
+   * @ngdoc service
+   * @name codepen
+   * @description
+   * Provides a service to open a code example in codepen.
+   */
   function Codepen($demoAngularScripts, $document, $codepenDataAdapter) {
 
     var CODEPEN_API = 'http://codepen.io/pen/define/';
@@ -11,6 +17,23 @@
       editOnCodepen: editOnCodepen
     };
 
+    /**
+     * @ngdoc function
+     * @name $codepen.editOnCodepen
+     * @param {object} demo The object representing the metadata and contents for a demo
+     * @usage
+     * $codepen.editOnCodepen({
+     *   id: 'demo-id',
+     *   title: 'title',
+     *   module: 'module-name',
+     *   files: {
+     *     html: [{ name: 'relative-path', contents: '...'}],
+     *     css: [{ name: 'relative-path', contents: '...'],
+     *     index: { contents: '...' },
+     *     js: [{ contents: '...' }]
+     *   }
+     * });
+     */
     function editOnCodepen(demo) {
       var data = $codepenDataAdapter.translate(demo, $demoAngularScripts.all());
       var form = buildForm(data);
@@ -33,6 +56,14 @@
     };
   };
 
+  /**
+   * @ngdoc service
+   * @description
+   * ** INTENDED FOR INTERNAL USE **
+   *
+   * Translates demo metadata and files into Codepen's post form data.  See api documentation for
+   * additional fields not used by this service. (http://blog.codepen.io/documentation/api/prefill)
+   */
   function CodepenDataAdapter() {
 
     var CORE_JS = 'https://cdn.rawgit.com/angular/bower-material/master/angular-material.js';
@@ -43,6 +74,28 @@
       translate: translate
     };
 
+    /**
+     * @ngdoc function
+     * @name $codepenDataAdapter.translate
+     * @param {object} demo The object representing the metadata and contents for a demo
+     * @param {Array} externalScripts Any additional dependent scripts
+     * @usage
+     * var demo = {
+     *   id: 'demo-id',
+     *   title: 'title',
+     *   module: 'module-name',
+     *   files: {
+     *     html: [{ name: 'relative-path', contents: '...'}],
+     *     css: [{ name: 'relative-path', contents: '...'],
+     *     index: { contents: '...' },
+     *     js: [{ contents: '...' }]
+     *    }
+     * };
+     *
+     * var externalScripts = ['pathToScript1', 'pathToScript2'];
+     *
+     * $codepen.editOnCodepen(demo, externalScripts);
+     */
     function translate(demo, externalScripts) {
       var files = demo.files;
 
@@ -84,6 +137,8 @@
       });
     };
 
+    //Adds class to parent element so that styles are applied correctly
+    //Adds ng-app attribute.  This is the same module name provided in the asset-cache.js
     function applyAngularAttributesToParentElement(html, demo) {
       var tmp = angular.element(html);
       tmp.addClass(demo.id);
@@ -91,6 +146,7 @@
       return tmp[0].outerHTML;
     };
 
+    //Adds templates inline in the html, so that templates are cache in the example
     function insertTemplatesAsScriptTags(indexHtml, demo) {
       if (demo.files.html.length) {
         var tmp = angular.element(indexHtml);
@@ -102,12 +158,15 @@
       return indexHtml;
     };
 
+    //Escapes ampersands so that after codepen unescapes the html the escaped code block
+    //uses the correct escaped characters
     function replaceEscapedCharacters(html) {
       return html
         .replace(/&gt;/g, "&amp;gt;")
         .replace(/&lt;/g, "&amp;lt;");
     };
 
+    //Required to make codepen work.  Without replacing the module, the asset cache will not work
     function replaceDemoModuleWithCodepenModule(file) {
       var matchAngularModule =  /\.module\(('[^']*'|"[^"]*")\s*,(?:\s*\[([^\]]*)\])?/g;
       return file.replace(matchAngularModule, ".module('MyApp'");
